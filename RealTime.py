6652e9f3f45c8fb8ee89dae3b6a7c18d31e4a199
ojames22 <66024854+ojames22@users.noreply.github.com>
2020-12-02 11:46:13 -0500

    Update RealTime.py

--- RealTime.py
+++ RealTime.py
@@ -2,25 +2,35 @@
 #Secret_Key = 'EWUK7R5ZnF9aou7AwhrRYlkqE3dAcM1JKhSP2Vqm'
 
 #https://www.youtube.com/watch?v=Mv6c_9FqNx4
-
-import config
+import json
 import websocket
 
-def open_stream(ws):
-	print('Opened')
-	auth_data = {
-		'action': 'authenticate'
-		'data': {'key_id': config.API_Key,'secret_key': config.Secret_Key}
-	} #Signals that the interface is ready to recieve data, logs into Alpaca with personal key id and secret key
+API_Key = 'PKRJJ2QQ0IU0TXNKXFCU'
+Secret_Key = 'EWUK7R5ZnF9aou7AwhrRYlkqE3dAcM1JKhSP2Vqm'
 
+def open_stream(ws):
+	print("Opened")
+	auth_data = {
+		"action": "auth"
+		"params": API_Key
+		#"data": {'key_id': API_Key,'secret_key': Secret_Key}
+	} #Signals that the interface is ready to recieve data, logs into Alpaca with personal key id and secret key
+	#creates variable auth_data, defienes course of action in order to log into alpaca 
 	ws.send(json.dumps(auth_data)) #sends message, .dumps converts it to json stream. json - receives, stores, and transports data
 
-	listen_message = {'action': 'listen','data':['streams: [AM.TSLA]']}}
+	#channel_data = {
+		#"action": "subscribe"
+		#"params": "AM.TSLA"
+	#}
+	listen_message = {'action': 'listen','data':['streams: [AM.TSLA]']}} #TSLA specific (or any ticker) 'listens' to 'data' in the
+	#TSLA stream
 
 	ws.send(json.dumps(listen_message))
+	#websocket creates a two way stream between the server and terminal/browser, 'dumps' variable listen_message into new enviroment
+	#the code 'ws.send(json.dumps(...)' is important to pulling data and displaying data because it signals the relationship
+	#between the stream and my terminal
 
-
-def on_message(ws, message):
+def on_message(ws, message): #the websocket recieves a message and that message is displayed
 	print('Recieved a message')
 	print(message)
 
@@ -28,10 +38,10 @@ def on_close(ws):
 	print('Closed connection')
 
 
-socket = 'wss://data.alpaca.markets.stream'
+socket = 'wss://data.alpaca.markets.stream' 
 
 ws = websocket.WebSocketApp(socket, open_stream=open_stream, on_message=on_message)
-ws.run_forever() = 
+ws.run_forever()
 
 #open_stream()
 #on_message()
