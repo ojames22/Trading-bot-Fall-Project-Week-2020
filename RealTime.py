bfde2851563fc4abc3fba13ff33641672d059d4b
ojames22 <66024854+ojames22@users.noreply.github.com>
2020-12-04 12:12:21 -0500

    Update RealTime.py

--- RealTime.py
+++ RealTime.py
@@ -1,43 +1,48 @@
-#API_Key = 'PKRJJ2QQ0IU0TXNKXFCU'
-#Secret_Key = 'EWUK7R5ZnF9aou7AwhrRYlkqE3dAcM1JKhSP2Vqm'
-
-#https://www.youtube.com/watch?v=Mv6c_9FqNx4
+import config
 import websocket, json
-#import json
 
-API_Key = "PKRJJ2QQ0IU0TXNKXFCU"
-Secret_Key = "EWUK7R5ZnF9aou7AwhrRYlkqE3dAcM1JKhSP2Vqm"
+#API_Key = "PKRJJ2QQ0IU0TXNKXFCU"
+#Secret_Key = "EWUK7R5ZnF9aou7AwhrRYlkqE3dAcM1JKhSP2Vqm"
 
 def on_open(ws):
 	print("Opened")
 	auth_data = {
-		"action": "auth",
-		"data": {"key_id": API_Key, "secret_key": Secret_Key}
-	} #Signals that the interface is ready to recieve data, logs into Alpaca with personal key id and secret key
+		"action": "authenticate",
+		"data": {"key_id": config.API_Key, "secret_key": config.Secret_Key}
+	} 
+
+	ws.send(json.dumps(auth_data)) 
+
+	listen_message = {"action": "listen", "data": {"streams": ["AM.TSLA"]}}
+
+	ws.send(json.dumps(listen_message))
+
+
+def on_message(ws, message): 
+	print("Recieved a message")
+	print(message)
+
+def on_close(ws):
+	print("Closed connection")
+
+
+socket = "wss://data.alpaca.markets/stream" 
+
+ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
+ws.run_forever()
+
+
+#https://www.youtube.com/watch?v=Mv6c_9FqNx4
+#Line 10: Signals that the interface is ready to recieve data, logs into Alpaca with personal key id and secret key
 	#creates variable auth_data, defienes course of action in order to log into alpaca 
-	ws.send(json.dumps(auth_data)) #sends message, .dumps converts it to json stream. json - receives, stores, and transports data
+#Line 13: sends message, .dumps converts it to json stream. json - receives, stores, and transports data
+
 
 	#channel_data = {
 		#"action": "subscribe"
 		#"params": "AM.TSLA"
 	#}
-	listen_message = {"action": "listen", "data": {"streams": ["T.TSLA", "T.AAPL"]}} #TSLA specific (or any ticker) 'listens' to 'data' in the
-	#TSLA stream
-
-	ws.send(json.dumps(listen_message))
-	#websocket creates a two way stream between the server and terminal/browser, 'dumps' variable listen_message into new enviroment
-	#the code 'ws.send(json.dumps(...)' is important to pulling data and displaying data because it signals the relationship
-	#between the stream and my terminal
-
-def on_message(ws, message): #the websocket recieves a message and that message is displayed
-	print("Recieved a message")
-	print(message)
-
-#def on_close(ws):
-	#print("Closed connection")
-
-
-socket = "wss://data.alpaca.markets/stream" 
-
-ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
-ws.run_forever()
+#Line 17: websocket creates a two way stream between the server and terminal/browser, 'dumps' variable listen_message into new enviroment
+#the code 'ws.send(json.dumps(...)' is important to pulling data and displaying data because it signals the relationship
+#between the stream and my terminal
+#Line 19: the websocket recieves a message and that message is displayed
