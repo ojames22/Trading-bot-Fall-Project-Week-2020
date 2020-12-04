3f40e4238633cf5b8533123b1a782ae9d62caeff
ojames22 <66024854+ojames22@users.noreply.github.com>
2020-12-03 21:54:45 -0500

    Create Parameters.py
    Only a general idea of a parameters function

---
+++ Parameters.py
@@ -0,0 +1,22 @@
+#import alpaca_trade_api as tradeapi
+#from alpaca_trade_api import StreamConn
+
+#class PythonTradingBot :
+
+def rec_doji
+
+	if "streams" = ("streams" * 0.98) #after one minute
+		"streams" = bar.close
+		#create_order("TSLA", 125, "buy", "market", "gtc")
+
+	if "streams" = ("streams" * 1.02)
+		"streams" = bar.open
+		#create_order("TSLA", 125, "sell", "market", "gtc")
+
+	if bar.close >= bar.open and bar.open - bar.close > 0.01:
+		print("Order recieved: () shares of (ticker)")
+		self.alpaca.submit_order
+
+#bd=PythonTradingBot()
+#bd.run()
+rec_doji()
